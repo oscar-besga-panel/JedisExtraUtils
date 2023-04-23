2a467cbf9c86847f2e922084410e29f227bd0916
oscar-besga-panel <oscar.besga@panel.es>
2023-04-23 20:54:57 +0200

    difficult LUA scripting

--- src/main/java/org/oba/jedis/extra/utils/rateLimiter/BucketRateLimiter.java
+++ src/main/java/org/oba/jedis/extra/utils/rateLimiter/BucketRateLimiter.java
@@ -3,8 +3,13 @@ package org.oba.jedis.extra.utils.rateLimiter;
 import org.oba.jedis.extra.utils.utils.JedisPoolUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
 
+import java.math.BigInteger;
+import java.time.Duration;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -45,53 +50,95 @@ public class BucketRateLimiter implements JedisPoolUser {
         return jedisPool;
     }
 
+    public void create(long capacity, Mode mode, long timeToRefillMillis) {
+        this.create(capacity, mode, timeToRefillMillis, TimeUnit.MILLISECONDS);
+    }
+
     public void create(long capacity, Mode mode, long timeToRefill, TimeUnit timeUnit) {
         withJedisPoolDo( jedis -> {
+            createWithJedis(jedis, capacity, mode, timeToRefill, timeUnit);
+        });
+    }
+
+    public static final BigInteger BI_MILLION = BigInteger.valueOf(1_000_000L);
+    public static final BigInteger BI_THOUSAND = BigInteger.valueOf(1_000L);
+
+
+
+    private void createWithJedis(Jedis jedis, long capacity, Mode mode, long timeToRefill, TimeUnit timeUnit) {
+        if (!jedis.exists(name)) {
             List<String> tmp = jedis.time();
-            double timeToRefillMicros = timeUnit.toMillis(timeToRefill) * 1000.0;
-            double redisTimestampMicros = (Long.parseLong(tmp.get(0)) * 1000.0) + Long.parseLong(tmp.get(1));
+            BigInteger timeToRefillMicros = BigInteger.valueOf(timeUnit.toMillis(timeToRefill)).multiply(BI_THOUSAND);
+            BigInteger redisTimestampMicros = BigInteger.valueOf(Long.parseLong(tmp.get(0))).multiply(BI_MILLION).
+                    add(BigInteger.valueOf(Long.parseLong(tmp.get(1))));
             Map<String, String> internalData = new HashMap<>();
             internalData.put(CAPACITY, Long.toString(capacity));
             internalData.put(AVAILABLE, Long.toString(capacity));
-            internalData.put(REFILL_MICROS, Double.toString(timeToRefillMicros));
+            internalData.put(REFILL_MICROS, timeToRefillMicros.toString());
             internalData.put(MODE, mode.name().toLowerCase());
-            internalData.put(LAST_REFILL_MICROS, Double.toString(redisTimestampMicros));
+            internalData.put(LAST_REFILL_MICROS, redisTimestampMicros.toString());
             jedis.hset(name, internalData);
             LOGGER.debug("created with capacity {} mode {} timeToRefill {} timeUnit {} in redisTimestampMicros {}",
                     capacity, mode, timeToRefill, timeUnit, redisTimestampMicros);
-        });
+        }
+    }
+
+    public boolean exists() {
+        return withJedisPoolGet( j -> j.exists(name));
     }
 
     public boolean acquire() {
         return acquire(1L);
     }
 
+    public static final Long LUA_1_TRUE = Long.valueOf(1L);
+    public static final String LUA_1_TRUE_STRING = "1";
+
 
     public boolean acquire(long permits) {
-        Object result = withJedisPoolGet(jedis -> {
-            return jedis.eval(ACQUIRE, Collections.singletonList(name), Collections.singletonList(Long.toString(permits)));
-        });
-        return Boolean.parseBoolean(result.toString());
+        Object result = withJedisPoolGet(jedis ->
+                jedis.eval(ACQUIRE, Collections.singletonList(name), Collections.singletonList(Long.toString(permits)))
+        );
+        if (result == null) {
+            return false;
+        } else if (result.equals(LUA_1_TRUE) || result.equals(LUA_1_TRUE_STRING)) {
+            return true;
+        } else {
+            return Boolean.parseBoolean(result.toString());
+        }
     }
 
 
 
     public static final String ACQUIRE = "" +
+            "" + "\n" +
             "local name = KEYS[1]" + "\n" +
-            "local permits = ARGS[1]"  + "\n" +
+            "local permits = tonumber(ARGV[1])"  + "\n" +
         "redis.log(redis.LOG_WARNING, 'name ' .. name .. ' permits ' .. permits)"  + "\n" +
-        "redis.call(ECHO, 'name ' .. name .. ' permits ' .. permits)"  + "\n" +
+        "redis.call('ECHO', 'name ' .. name .. ' permits ' .. permits)"  + "\n" +
             "-- refill" + "\n" +
             "local tst = redis.call('time')" + "\n" +
-            "local ts = tst[0] * 1000000 + tst[1]" + "\n" +
+            "local ts = tst[1] * 1000000 + tst[2]" + "\n" +
             "local refill = false" + "\n" +
             "local mode = redis.call('hget', name, 'mode')"  + "\n" +
         "redis.log(redis.LOG_WARNING, 'ts ' .. ts .. ' mode ' .. mode)"  + "\n" +
-        "redis.call(ECHO, 'ts ' .. ts .. ' mode ' .. mode)"  + "\n" +
-
-            "" + "\n" +
+        "redis.call('ECHO', 'ts ' .. ts .. ' mode ' .. mode)"  + "\n" +
+            "if refill then"  + "\n" +
+        "redis.log(redis.LOG_WARNING, 'refill ok ')"  + "\n" +
+        "redis.call('ECHO', 'refill ok')"  + "\n" +
+            "end" + "\n" +
             "-- try acquire" + "\n" +
             "local acquire = false" + "\n" +
+            "local available = tonumber(redis.call('hget', name, 'available'))" + "\n" +
+          "redis.log(redis.LOG_WARNING, 'available ' .. available .. ' permits ' .. permits)"  + "\n" +
+          "redis.call('ECHO',  'available ' .. available .. ' permits ' .. permits)"  + "\n" +
+            "if (available >= permits) then" + "\n" +
+            "  available = available - permits" + "\n" +
+            "  redis.call('hset', name, 'available', available)" + "\n" +
+            "  acquire = true" + "\n" +
+          "redis.log(redis.LOG_WARNING, 'available ' .. available .. ' acquire ' .. tostring(acquire))"  + "\n" +
+          "redis.call('ECHO',  'available ' .. available .. ' acquire ' .. tostring(acquire))"  + "\n" +
+            "end" + "\n" +
             "return acquire";
 
     /**
@@ -120,6 +167,40 @@ public class BucketRateLimiter implements JedisPoolUser {
         }
     }
  */
+    public static void main(String[] args) {
+
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(24); // original 128
+        jedisPoolConfig.setMaxIdle(24); // original 128
+        jedisPoolConfig.setMinIdle(4); // original 16
+        // High performance
+//        jedisPoolConfig.setMaxTotal(128);
+//        jedisPoolConfig.setMaxIdle(128);
+//        jedisPoolConfig.setMinIdle(16);
+        jedisPoolConfig.setTestOnBorrow(true);
+        jedisPoolConfig.setTestOnReturn(true);
+        jedisPoolConfig.setTestWhileIdle(true);
+        jedisPoolConfig.setMinEvictableIdleTimeMillis(Duration.ofSeconds(30).toMillis());
+        jedisPoolConfig.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(10).toMillis());
+        jedisPoolConfig.setNumTestsPerEvictionRun(1);
+        jedisPoolConfig.setBlockWhenExhausted(true);
+        String host = "127.0.0.1";
+        int port = 6379;
+        String pass = "";
+        JedisPool jedisPool = null;
+        if (pass != null && !pass.trim().isEmpty()) {
+            jedisPool = new JedisPool(jedisPoolConfig, host, port, Protocol.DEFAULT_TIMEOUT, pass);
+        } else {
+            jedisPool = new JedisPool(jedisPoolConfig, host, port);
+        }
+        BucketRateLimiter bucketRateLimiter = new BucketRateLimiter(jedisPool, "bucketRateLimiter:test_" + System.currentTimeMillis());
+        bucketRateLimiter.create(10, Mode.GREEDY, 1, TimeUnit.SECONDS);
+        boolean result1 = bucketRateLimiter.acquire();
+        boolean result100 = bucketRateLimiter.acquire(100);
+
+        System.out.println("Acquire result " + result1);
+        System.out.println("Acquire result " + result100);
+    }
 
 
 
