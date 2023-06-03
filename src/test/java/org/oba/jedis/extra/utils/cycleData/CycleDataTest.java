9cec5d40cc21741f54e8ae8bc5d470ff055466ad
oscar-besga-panel <oscar.besga@panel.es>
2023-06-03 16:39:27 +0200

    cycle data with test

---
+++ src/test/java/org/oba/jedis/extra/utils/cycleData/CycleDataTest.java
@@ -0,0 +1,95 @@
+package org.oba.jedis.extra.utils.cycleData;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.oba.jedis.extra.utils.cycle.CycleData;
+import redis.clients.jedis.JedisPool;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
+
+public class CycleDataTest {
+
+    private MockOfJedis mockOfJedis;
+    private JedisPool jedisPool;
+    private String cycleDataName;
+
+    @Before
+    public void before() throws IOException {
+        org.junit.Assume.assumeTrue(MockOfJedis.unitTestEnabled());
+        if (!MockOfJedis.unitTestEnabled()) return;
+        mockOfJedis = new MockOfJedis();
+        mockOfJedis.setDoWithEvalSha((r,k,p) -> cycleDataExecution(k,p));
+        jedisPool = mockOfJedis.getJedisPool();
+        cycleDataName = "cycleDataName:" + this.getClass().getName() + ":" + System.currentTimeMillis();
+    }
+
+    @After
+    public void after() throws IOException {
+        jedisPool.close();
+        mockOfJedis.clearData();
+    }
+
+    String cycleDataExecution(List<String> keys, List<String> values) {
+        Map<String, String> cycle = mockOfJedis.getData(keys.get(0));
+        int current = Integer.parseInt(cycle.get("current"));
+        int size = cycle.size() - 1;
+        String result = cycle.get(Integer.toString(current));
+        current++;
+        if (current >= size) {
+            current = 0;
+        }
+        cycle.put("current",Integer.toString(current));
+        return result;
+    }
+
+    @Test
+    public void cycleDataBasicTest() throws InterruptedException {
+        CycleData cycleData = new CycleData(jedisPool, cycleDataName).
+                create("A","B","C");
+        List<String> results = new ArrayList<>();
+        for(int i = 0; i < 10; i++) {
+            String result = cycleData.next();
+            results.add(result);
+        }
+        List<String> compare = Arrays.asList("A","B","C","A","B","C","A","B","C","A");
+        for(int i = 0; i < 10; i++) {
+            assertEquals(compare.get(i), results.get(i));
+        }
+    }
+
+    @Test
+    public void createBasicTest() throws InterruptedException {
+        CycleData cycleData = new CycleData(jedisPool, cycleDataName).
+                createIfNotExists("A","B","C");
+        assertTrue(cycleData.exists());
+        cycleData.delete();
+        assertFalse(cycleData.exists());
+        cycleData.createIfNotExists("C","B","A");
+        assertTrue(cycleData.exists());
+        cycleData.createIfNotExists("C","B","A");
+        assertTrue(cycleData.exists());
+    }
+
+    @Test
+    public void iteratorTest() {
+        CycleData cycleData = new CycleData(jedisPool, cycleDataName).
+                createIfNotExists("A","B","C");
+        int num = 0;
+        // Tried with one million and it works
+        while(cycleData.hasNext() && (num++ < 1_000)){
+            assertNotNull(cycleData.next());
+        }
+        assertNotNull(cycleData.next());
+    }
+
+}
---
+++ src/test/java/org/oba/jedis/extra/utils/cycleData/MockOfJedis.java
@@ -0,0 +1,128 @@
+package org.oba.jedis.extra.utils.cycleData;
+
+import org.mockito.Mockito;
+import org.oba.jedis.extra.utils.utils.ScriptEvalSha1;
+import org.oba.jedis.extra.utils.utils.TriFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+public class MockOfJedis {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MockOfJedis.class);
+
+    public static final String CLIENT_RESPONSE_OK = "OK";
+    public static final String CLIENT_RESPONSE_KO = "KO";
+
+
+    // Zero to prevent some unit test
+    // One to one pass
+    // More to more passes
+    static final int UNIT_TEST_CYCLES = 1;
+
+    static boolean unitTestEnabled(){
+        return UNIT_TEST_CYCLES > 0;
+    }
+
+    private final JedisPool jedisPool;
+    private final Jedis jedis;
+    private final Map<String, Map<String,String>> data = Collections.synchronizedMap(new HashMap<>());
+
+    private TriFunction<String, List<String>, List<String>, Object> doWithEvalSha;
+
+    public MockOfJedis() {
+
+        jedis = Mockito.mock(Jedis.class);
+        jedisPool = Mockito.mock(JedisPool.class);
+        when(jedis.exists(anyString())).thenAnswer( ioc -> {
+            String name = ioc.getArgument(0, String.class);
+            return exists(name);
+        });
+        when(jedis.del(anyString())).thenAnswer( ioc -> {
+            String name = ioc.getArgument(0, String.class);
+            return delete(name);
+        });
+        when(jedis.hset(anyString(), any(Map.class))).thenAnswer( ioc ->  {
+            String name = ioc.getArgument(0, String.class);
+            Map<String, String> map = ioc.getArgument(1, Map.class);
+            return hset(name, map);
+        });
+        when(jedis.hset(anyString(), anyString(), anyString())).thenAnswer( ioc ->  {
+            String name = ioc.getArgument(0, String.class);
+            String key = ioc.getArgument(1, String.class);
+            String value = ioc.getArgument(2, String.class);
+            return hset(name, key, value);
+        });
+        when(jedis.scriptLoad(anyString())).thenAnswer( ioc -> {
+            String script = ioc.getArgument(0, String.class);
+            return ScriptEvalSha1.sha1(script);
+        });
+        when(jedis.evalsha(anyString(), any(List.class), any(List.class))).thenAnswer( ioc -> {
+            String name = ioc.getArgument(0, String.class);
+            List<String> keys = ioc.getArgument(1, List.class);
+            List<String> args = ioc.getArgument(2, List.class);
+            return executeSha(name, keys, args);
+        });
+        Mockito.when(jedisPool.getResource()).thenReturn(jedis);
+    }
+
+
+    private Object executeSha(String name, List<String> keys, List<String> args) {
+        return doWithEvalSha != null ? doWithEvalSha.apply(name, keys, args) : null;
+    }
+
+    private Boolean exists(String name) {
+        return data.containsKey(name);
+    }
+
+    private Long hset(String name, String key, String value) {
+        data.computeIfAbsent(name, k -> new HashMap<>()).put(key, value);
+        return 1L;
+    }
+
+    private Long hset(String name, Map<String, String> map) {
+        data.computeIfAbsent(name, k -> new HashMap<>()).putAll(map);
+        return 1L;
+    }
+
+    private Object delete(String name) {
+        Map<String, String> removed = data.remove(name);
+        return removed != null ? 1L : 0L;
+    }
+
+
+    public void setDoWithEvalSha(TriFunction<String, List<String>, List<String>, Object> doWithEvalSha) {
+        this.doWithEvalSha = doWithEvalSha;
+    }
+
+    public Jedis getJedis(){
+        return jedis;
+    }
+
+    public JedisPool getJedisPool(){
+        return jedisPool;
+    }
+
+    public synchronized void clearData(){
+        data.clear();
+        doWithEvalSha = null;
+    }
+
+    public Map<String, String> getData(String name) {
+        return data.get(name);
+    }
+
+
+
+}
